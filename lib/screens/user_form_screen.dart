f7088ace88a151862344000bffa5d2fb8646fe53
unknown <djg.pujota@yavirac.edu.ec>
2022-07-15 02:12:03 -0500

    more

--- lib/screens/user_form_screen.dart
+++ lib/screens/user_form_screen.dart
@@ -15,15 +15,53 @@ class UserFormScreen extends StatelessWidget {
         child: Column(children: const [
           CustomInputField(
             
-            t1: 'ola', //hintText
-            t2: 'ola2', // labelText:
-            t3: 'eola3', //helperText
-            se: Icons.supervised_user_circle_rounded,//Suffixicon
-            sa: Icons.supervised_user_circle_rounded,//Icon
+            t1: 'Escribe tu nombre', //hintText
+            t2: 'Nombre', // labelText:
+            t3: 'Only Worlds', //helperText
+            se: Icons.people,//Suffixicon
+            sa: Icons.people,//Icon
+            autof: true, //Autofocus
+            keyboardType: TextInputType.text, //keyboardtype
+            obscureTexto: false, //ObscureText
+            ),
+
+            CustomInputField(
+            
+            t1: 'Escribe tu last name', //hintText
+            t2: 'Apeshido', // labelText:
+            t3: 'Only Worlds', //helperText
+            se: Icons.people,//Suffixicon
+            sa: Icons.people,//Icon
+            autof: true, //Autofocus
+            keyboardType: TextInputType.text, //keyboardtype
+            obscureTexto: false, //ObscureText
+            ),
+
+            CustomInputField(
+            
+            t1: 'Escribe tu email ', //hintText
+            t2: 'Emei', // labelText:
+            t3: '@example.com', //helperText
+            se: Icons.email_outlined,//Suffixicon
+            sa: Icons.email,//Icon
+            autof: true, //Autofocus
+            keyboardType: TextInputType.emailAddress, //keyboardtype
+            obscureTexto: false, //ObscureText
+            ),
+
+            CustomInputField(
+            
+            t1: 'Password  ', //hintText
+            t2: 'Contra ', // labelText:
+            t3: 'Aeaemamaniema', //helperText
+            se: Icons.password,//Suffixicon
+            sa: Icons.password,//Icon
             autof: true, //Autofocus
             keyboardType: TextInputType.text, //keyboardtype
             obscureTexto: true, //ObscureText
             ),
+
+            ElevatedButton(onPressed: null, child: const Text('obrigado'))
         ]),
       ),
     );
